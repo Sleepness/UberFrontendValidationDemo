d46bb29ca2b5895b8f72f3f10bad6e8d4a8ae43b
alexandrzhulev <alexandrzhulev@gmail.com>
2015-04-02 00:41:22 +0300

    added extra fields to post model

--- src/Acme/DemoBundle/Model/Post.php
+++ src/Acme/DemoBundle/Model/Post.php
@@ -14,19 +14,123 @@ class Post
 {
     /**
      * @Assert\NotBlank(message="Title should not be blank!")
-     * @Assert\Length(min=8, minMessage = "This value should be longer than 8 chars")
+     * @Assert\Length(min=8, minMessage="This value should be longer than 8 chars")
      */
     private $title;
 
     /**
+     * @Assert\NotNull()
+     * @Assert\Length(min=20)
+     */
+    private $content;
+
+    /**
+     * @Assert\Date()
+     */
+    private $date;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(max=10)
+     */
+    private $authorName;
+
+    /**
+     * @Assert\Email()
+     */
+    private $authorEmail;
+
+    /**
+     * @Assert\Url()
+     */
+    private $authorHomePage;
+
+    /**
+     * @param $authorEmail
+     */
+    public function setAuthorEmail($authorEmail)
+    {
+        $this->authorEmail = $authorEmail;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthorEmail()
+    {
+        return $this->authorEmail;
+    }
+
+    /**
+     * @param $authorHomePage
+     */
+    public function setAuthorHomePage($authorHomePage)
+    {
+        $this->authorHomePage = $authorHomePage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthorHomePage()
+    {
+        return $this->authorHomePage;
+    }
+
+    /**
+     * @param $authorName
+     */
+    public function setAuthorName($authorName)
+    {
+        $this->authorName = $authorName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthorName()
+    {
+        return $this->authorName;
+    }
+
+    /**
+     * @param $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
      * @param $title
-     * @return $this
      */
     public function setTitle($title)
     {
         $this->title = $title;
-
-        return $this;
     }
 
     /**
