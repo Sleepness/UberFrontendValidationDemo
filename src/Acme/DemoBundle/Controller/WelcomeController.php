b7268ffb43139e4a09ea62005cbee4391b285594
alexandrzhulev <alexandrzhulev@gmail.com>
2015-04-01 16:40:58 +0300

    added simple form to view

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -2,12 +2,29 @@
 
 namespace Acme\DemoBundle\Controller;
 
+use Acme\DemoBundle\Form\Type\PostType;
+use Acme\DemoBundle\Model\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class WelcomeController extends Controller
 {
-    public function indexAction()
+    /**
+     * Render form for inserting post information
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
     {
-        return $this->render('AcmeDemoBundle:Welcome:index.html.twig');
+        $post = new Post();
+        $form = $this->createForm(new PostType(), $post);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            // do something
+        }
+        return $this->render('AcmeDemoBundle:Welcome:index.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
